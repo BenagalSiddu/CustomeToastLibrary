b598a8355a5c5d6464f937a4ec2d389fffccf8d4
Admin <mydentalplan.in@gmail.com>
2019-12-16 14:59:24 +0530

    by siddu on 16/12/2019 14:44 pm

--- app/src/main/java/com/siddu/custometoastactivity/MainActivity.java
+++ app/src/main/java/com/siddu/custometoastactivity/MainActivity.java
@@ -14,8 +14,6 @@ import android.widget.Toast;
 import com.siddu.custometoastlibrary.CustomeToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_tost;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Custome Toast");
         setSupportActionBar(toolbar);
-        btn_tost = findViewById(R.id.btn_tost);
-        btn_tost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               /* LayoutInflater inflater = getLayoutInflater();
-                View view = inflater.inflate(R.layout.custome_toast_layout, (ViewGroup) findViewById(R.id.main_ll));
-                TextView txt_toast = view.findViewById(R.id.txt_toast);
-                ImageView img_toast = view.findViewById(R.id.img_toast);
-                txt_toast.setText("An Android Toast is a small message displayed on the screen, similar to a tool tip or other similar popup notification.");
-                img_toast.setImageResource(R.drawable.custometoast);
-                Toast toast = new Toast(MainActivity.this);
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(view);
-                toast.show();*/
-                CustomeToasterMessage message = new CustomeToasterMessage(MainActivity.this);
-                message.ToastMesaage("An Android Toast is a small message displayed on the screen, similar to a tool tip or other similar popup notification.", R.drawable.custometoast);
-            }
-        });
+        CustomeToasterMessage message = new CustomeToasterMessage(MainActivity.this);
+        message.ToastMesaage("An Android Toast is a small message displayed on the screen, similar to a tool tip or other similar popup notification.", R.drawable.custometoast);
+
     }
 }
